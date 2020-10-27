d92ca5a8c51db9be15e69d8c15210d1e18e95aa8
Gregory Demont - M16946 <gregory.demont@microchip.com>
2020-10-27 13:58:45 +0100

    updated software folder

--- software/WLR089U0_P2P_v1.0/Debug/src/ASF/thirdparty/wireless/LoRa_P2P/source/miwi_p2p_star/miwi_p2p_pds.d
+++
@@ -1,3 +0,0 @@
-src/ASF/thirdparty/wireless/LoRa_P2P/source/miwi_p2p_star/miwi_p2p_pds.d \
- src/ASF/thirdparty/wireless/LoRa_P2P/source/miwi_p2p_star/miwi_p2p_pds.o: \
- ../src/ASF/thirdparty/wireless/LoRa_P2P/source/miwi_p2p_star/miwi_p2p_pds.c
